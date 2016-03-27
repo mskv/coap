fd03f0ed200ec3351eb29c6f10840decd0fe3379
Michal Podworny <michal.podworny@monterail.com>
2016-03-27 22:24:43 +0200

    Add gen_coap and apex dependencies

--- mix.exs
+++ mix.exs
@@ -10,23 +10,14 @@ defmodule Coap.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:gen_coap, git: "https://github.com/gotthardp/gen_coap.git"},
+      {:apex, "~>0.4.0"}
+    ]
   end
 end
