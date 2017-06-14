b9a3517b137a42ddc4197ce7f75fdd4087705211
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-06-14 12:11:00 +0200

    Update apex

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Coap.Mixfile do
   defp deps do
     [
       {:gen_coap, git: "https://github.com/gotthardp/gen_coap.git"},
-      {:apex, "~>0.4.0"}
+      {:apex, "~> 1.0.0"}
     ]
   end
 end
